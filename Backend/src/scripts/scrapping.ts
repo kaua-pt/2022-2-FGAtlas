1fca976f3429f0e658e92e2fab2382c4213fe8d5
StrangeUnit28 <rflbosi@gmail.com>
2023-01-17 10:32:35 -0300

    Algumas alterações para extender o Scrapper

--- Backend/src/scripts/scrapping.ts
+++ Backend/src/scripts/scrapping.ts
@@ -4,8 +4,10 @@ import generatePrisma from "./generatePrisma";
 /* eslint-disable*/
 /* istanbul ignore next */
 async function scrap() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  const page_mat = await browser.newPage();
+  const page_cic = await browser.newPage();
 
   await page.goto("https://sigaa.unb.br/sigaa/public/turmas/listar.jsf");
   await page.select('select[id="formTurma:inputDepto"]', "673");
@@ -20,6 +22,36 @@ async function scrap() {
   await page.waitForSelector("#turmasAbertas > table > tbody > tr.linhaPar > td:nth-child(4)");
   await page.waitForSelector("#turmasAbertas > table > tbody > tr");
   await page.waitForTimeout(4000);
+
+  await page_mat.goto("https://sigaa.unb.br/sigaa/public/turmas/listar.jsf");
+  await page_mat.select('select[id="formTurma:inputDepto"]', "518");
+  await page_mat.$eval('input[id="formTurma:inputAno"]', (el: any) => (el.value = "2022"));
+  await page_mat.select('select[id="formTurma:inputPeriodo"]', "2");
+  await page_mat.click('input[name="formTurma:j_id_jsp_1370969402_11"]');
+
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr.agrupador > td > a > span");
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td.turma");
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td.nome");
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td:nth-child(8)");
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr.linhaPar > td:nth-child(4)");
+  await page_mat.waitForSelector("#turmasAbertas > table > tbody > tr");
+  await page_mat.waitForTimeout(4000);
+
+  await page_cic.goto("https://sigaa.unb.br/sigaa/public/turmas/listar.jsf");
+  await page_cic.select('select[id="formTurma:inputDepto"]', "508");
+  await page_cic.$eval('input[id="formTurma:inputAno"]', (el: any) => (el.value = "2022"));
+  await page_cic.select('select[id="formTurma:inputPeriodo"]', "2");
+  await page_cic.click('input[name="formTurma:j_id_jsp_1370969402_11"]');
+
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr.agrupador > td > a > span");
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td.turma");
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td.nome");
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr.linhaImpar > td:nth-child(8)");
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr.linhaPar > td:nth-child(4)");
+  await page_cic.waitForSelector("#turmasAbertas > table > tbody > tr");
+  await page_cic.waitForTimeout(4000);
+
+  // const materias_mat: String[] = await page_mat.evaluate();
 
   const codigoNome: String[] = await page.$$eval(
     "#turmasAbertas > table > tbody > tr.agrupador > td > a > span",
@@ -54,7 +86,7 @@ async function scrap() {
     /* eslint-disable*/
     console.log("Dados já cadastrados");
   }
-  await browser.close();
+  //await browser.close();
 }
 
 /* eslint-disable*/
